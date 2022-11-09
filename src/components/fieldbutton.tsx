3a99933188c83e19038c87eb014b6def5d25a259
Eugene Matsumura <eugene.bb1@gmail.com>
2022-11-09 16:20:54 +0100

    fix: :bug: make page more reponsive for mobile devices

--- src/components/fieldbutton.tsx
+++ src/components/fieldbutton.tsx
@@ -11,7 +11,7 @@ const FieldButton: Component<FieldButtonProps> = (props) => {
   return (
     <div
       onClick={props.onClick}
-      class="bg-gray-300 rounded min-w-[8rem] min-h-[8rem] md:min-w-[12rem] md:min-h-[12rem] lg:min-w-[16rem] lg:min-h-[16rem] m-2 flex justify-center items-center"
+      class="bg-gray-300 rounded min-w-[6rem] min-h-[6rem] sm:min-w-[8rem] sm:min-h-[8rem] md:min-w-[12rem] md:min-h-[12rem] lg:min-w-[16rem] lg:min-h-[16rem] m-2 flex justify-center items-center"
     >
       <Switch fallback={<></>}>
         <Match when={props.state === PlayerState.Cross}>
