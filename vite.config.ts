e0f885fa5a83fc33a3a49c5612c0c2b8e412df74
Eugene Matsumura <eugene.bb1@gmail.com>
2022-11-10 20:06:17 +0100

    feat: :sparkles: add logo and PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig, loadEnv } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return {
-    plugins: [solidPlugin()],
+    plugins: [
+      solidPlugin(),
+      VitePWA({
+        registerType: "autoUpdate",
+        injectRegister: "auto",
+      }),
+    ],
     server: {
       port: 3000,
     },
